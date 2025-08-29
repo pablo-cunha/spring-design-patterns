ee8f33aa8941cf93ec17e993b5e22977f63865cf
pablo-cunha <lcunha.pablo@gmail.com>
2025-08-29 11:03:18 -0300

    Inicializa projeto base

---
+++ src/test/java/com/estudies/desing_patterns_spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudies.desing_patterns_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesingPatternsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
